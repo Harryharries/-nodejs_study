3b6e47b631f808172f60179c387db19a0c38a398
Zhengyu Gu <zhengyugu@Zhengyus-MBP-2.lan>
2020-02-06 12:21:57 +0800

    add usage 'off' 'inherit'

--- events_test.js
+++ events_test.js
@@ -4,14 +4,41 @@ const EventEmitter = require('events');
 
 const events = new EventEmitter();
 
+// inherit the event------
+class MyObject extends EventEmitter{
+
+}
+
+const myObj = new MyObject();
+
+myObj.on('aaaa',function (data) {
+    console.log('mmmmmmmm',data)
+});
+
+myObj.emit('aaaa','hello');
+// ----------
+
+// binding events-------
 events.on('click',function (data) {
     console.log(data);
 });
 
-events.on('remove',function(data){
+// events.on('remove',function(data){
+//     console.log('remove',data.userId);
+// });
+
+function removeHandler(data){
     console.log('remove',data.userId);
-});
+}
+events.on('remove',removeHandler);
+//--------
 
 events.emit('click','cliccccked');
 
 events.emit('remove', {userId:'1234'});
+
+
+// off will cancel the binding event
+events.off('remove',removeHandler)
+
+events.emit('remove', {userId:'7777'});
