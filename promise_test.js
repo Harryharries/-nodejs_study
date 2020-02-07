3af34736eb46cd0dc58d5e48139b630724f62b8e
Zhengyu Gu <zhengyugu@Zhengyus-MBP-2.lan>
2020-02-07 11:45:40 +0800

    use promise to solve the multi-callback problem

--- promise_test.js
+++ promise_test.js
@@ -1,28 +1,34 @@
-// use promise to solve the multi-callback problem
-const promise = new Promise(function (resolve,reject) {
-
-    setTimeout(function(){
-        resolve('ok')
-    },3000)
-});
-
-async function getName(){
+var fs = require('fs');
+function getFileByPath(path) {
     return new Promise(function (resolve,reject) {
-
-        setTimeout(function(){
-            resolve('ok')
-        },1000)
+        fs.readFile(path, 'utf8', function (err, data) {
+            if(err){
+                reject(err);   //失败
+            }else{
+                resolve(data);    //成功
+            }
+        })
     });
 }
 
-async function main(){
-    let name = await getName();
-    console.log(name);
-}
-
-
-// promise.then(function (data) {
-//     console.log(data);
-// }).catch(function (data) {
-//     console.log('error',error);
-// });
+//由于then通过getFileByPath返回的是一个promise对象，所以可以继续.then串联调用（链式调用）
+getFileByPath('./readme')
+    .then(function(data){
+        console.log("成功："+data);
+        return getFileByPath('./readme');
+    },function(err){
+        console.log("失败："+err);
+        return getFileByPath('./readme');
+    })
+    .then(function(data){
+        console.log("成功："+data);
+        return getFileByPath('./learningnotes');
+    },function(err){
+        console.log("失败："+err);
+        return getFileByPath('./learningnotes');
+    })
+    .then(function(data){
+        console.log("成功："+data);
+    },function(err){
+        console.log("失败："+err);
+    });
