163468074d5eb6095bded09276ef041cf2f7677f
Zhengyu Gu <zhengyugu@Zhengyus-MacBook-Pro-2.local>
2020-02-09 14:30:05 +0800

    add post function for server

--- express/express_server.js
+++ express/express_server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 // express give a service side app
 const app = express();
+
+//parse the submitted data to json
+app.use(bodyParser.json());
+
 // express.static tell express where is the index
 app.use(express.static(__dirname + '/public'));
 
@@ -24,6 +29,12 @@ app.get('/get_students', function (request,response) {
         {name:'ccc',gender: 'm'},
     ];
     response.send(students);
+});
+
+app.post('/register_user', function (req,res) {
+    //req.body
+    console.log(req.body);
+    res.send('ok');
 });
 
 app.listen(3000, function(err) {
